ea278d4cde036e34cc2d5c710fff71312e61b0f9
Jan Renra Gloser <jan.renra.gloser@gmail.com>
2014-08-22 13:29:26 +0200

    Array iteration

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# C++ Benchmarks
+
+Some benchamrking code
---
+++ array_iteration/with_modern_for_loop.cxx
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+// 100'000'000 seems a good benchmarking size
+int main(int argc, char ** argv){
+  if(argc < 2){
+    std::cout << "Give me array size" << std::endl;
+    return 1;
+  }
+
+  int size = std::stoi(argv[1]);
+  int sum = 0;
+  std::vector<int> * vec = new std::vector<int>(size);
+
+  for(auto i : *vec){
+    sum += i;
+  }
+
+  delete vec;
+
+  return 0;
+}
